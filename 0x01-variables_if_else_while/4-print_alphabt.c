47d1c62dff09e92f86e8c7e7114592e902b1a635
kimathi <isaackimathi19@gmail.com>
2023-02-17 13:04:48 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase
+ * followed by a new line, except e and q
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+	{
+		if (ab != 'e' && ab != 'z')
+			putchar(ab);
+	}
+
+	pitchar('\n');
+
+	return (0);
