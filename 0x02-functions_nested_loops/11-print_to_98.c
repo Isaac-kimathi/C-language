aa8d1868d10e7185e79e6ad79ed98dc40d1f6976
kimathi <isaackimathi19@gmail.com>
2023-02-22 01:58:15 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 11-main.c 11-print_to_98.c -o 11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,16 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
-
-	if (n <= 98)
+	while (n < 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-		if (i == 98)
-		printf("%d\n", i);
-		else
-		printf("%d, ", i);
-		}
+	printf("%i, ", n);
+	n++;
 	}
-	else if (n >= 98)
+	while (n > 98)
 	{
-		for (j = n; j >= 98; j--)
-		{
-		if (j == 98)
-			printf("%d\n,", j);
-		else if (j == 98)
-			printf("%d, ", j);
-		}
+		printf("%i, ", n);
+		n--;
 	}
+	printf("98");
+	putchar('\n');
 }
