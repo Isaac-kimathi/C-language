d9434055b0bf4945496f5c942b1d0befebff2720
kimathi <isaackimathi19@gmail.com>
2023-02-22 07:06:55 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,6 @@
 /**
  * main - Prints the add of the even-valued
  * fibonacci numbers.
- *
  * Return: Always 0.
  */
 int main(void)
@@ -26,4 +25,3 @@ int main(void)
 	printf("%ld\n", afn);
 	return (0);
 }
-
