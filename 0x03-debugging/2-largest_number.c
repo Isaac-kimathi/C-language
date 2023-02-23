1ada9ff1bd9ea97e1770f63d8fcd68feca84317c
kimathi <isaackimathi19@gmail.com>
2023-02-23 12:00:11 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,11 +15,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && c > a)
 	{
 		largest = b;
 	}
