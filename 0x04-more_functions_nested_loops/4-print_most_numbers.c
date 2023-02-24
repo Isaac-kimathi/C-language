e5de040e2589187874e9c75c18da170bf85c1d15
kimathi <isaackimathi19@gmail.com>
2023-02-24 02:42:07 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,7 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
 for (c = 48; c < 58; c++)
 {
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
  * Return: 1 (success)
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(int c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
