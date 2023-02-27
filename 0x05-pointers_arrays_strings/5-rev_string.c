c82d22da03d90b8bfb3da4630b2b3f6fa23de88d
kimathi <isaackimathi19@gmail.com>
2023-02-27 06:55:34 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,13 @@
  */
 void rev_string(char *s)
 {
-	int 1, length 1, length 2;
+	int i, length 1, length 2;
 
 	length1 = 0;
 	length2 = 0;
 
 	while (s[length1] != '0')
-		length1++
+		length1++;
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
