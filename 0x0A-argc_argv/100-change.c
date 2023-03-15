3cdf207a0087eab49cf7e695146d36aa65aff847
Isaac-kimathi <isaackimathi19@gmail.com>
2023-03-16 00:20:51 +0300

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
-
-/**
- * coinConvert - Helper function that does all the mathematics
- * @f: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConvert(int f)
-{
-	int count = 0;
-
-	while (f != 0)
-	{
-	if (f % 10 == 9 || f % 10 == 7)
-	f -= 2;
-	else if (f % 25 == 0)
-	f -= 25;
-	else if (f % 10 == 0)
-	f -= 10;
-	else if (f % 5 == 0)
-	f -= 5;
-	else if (f % 2 == 0)
-	{
-	if (f % 10 == 6)
-	f -= 1;
-	else
-	f -= 2;
-	}
-	else
-	f -= 1;
-
-	count++;
-	}
-	return (count);
-}
 
 /**
  * main - Takes in exactly one argument for minimum coin count
@@ -46,9 +10,7 @@ int coinConvert(int f)
  */
 int main(int argc, char *argv[])
 {
-	int f, c;
-
-	c = 0;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
@@ -56,16 +18,34 @@ int main(int argc, char *argv[])
 	return (1);
 	}
 
-	f = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	if (f < 0)
-	printf("0\n");
-
-	else
+	while (cents > 0 )
 	{
-	c = coinConvert(f);
-
-	printf("%d\n", c);
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		 if ((cents - 10) >= 0)
+		{
+		       	cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
+	printf("%d\n", coins);
+
 	return (0);
 }
