297c229deb7af5b825084fa245b0f31edf9e9406
Isaac-kimathi <isaackimathi19@gmail.com>
2023-03-22 18:05:34 +0300

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
-
 	if (array == NULL && action == NULL)
 		return;
-	for (i = 0; i < size; i++)
+
+	while (size-- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 }
