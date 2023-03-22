5464747c4b88136ddc4f90565532dd363fc75026
Isaac-kimathi <isaackimathi19@gmail.com>
2023-03-22 19:18:17 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	i = -1;
+	if (size <= 0 || !(array) || !(cmp))
 		return (-1);
 
-	for  (i = 0; i < size; i++)
+	while (i++ < size)
 	{
-		if (cmp(array[i] != 0))
+		if (cmp(array[i]))
 			return (i);
 	}
 
