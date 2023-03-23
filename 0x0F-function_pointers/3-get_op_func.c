e281be3b062496a58fa3d194c4c6b5befd404aa8
Isaac-kimathi <isaackimathi19@gmail.com>
2023-03-23 18:08:55 +0300

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,14 +21,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].f) != NULL)
+	while (1 < 5)
 	{
+		if (ops[i].op[0] == s[0])
 		return (ops[i].f);
 		i++;
 	}
-	printf("Error\n");
-	exit(99);
 	return (0);
 }
-
-
